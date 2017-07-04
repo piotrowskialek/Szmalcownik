2ab5dea5d05fdae9cc2782880ab2f129a15c976c
piotrowskialek <piotrowskialek@icloud.com>
2017-07-05 00:26:01 +0200

    todo permissiony po gps

--- app/src/main/java/edu/eiti/apiotrowski/szmalcownik/Main.kt
+++ app/src/main/java/edu/eiti/apiotrowski/szmalcownik/Main.kt
@@ -28,22 +28,14 @@ class Main : FragmentActivity(), OnMapReadyCallback {
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
         val mlocManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
         val mlocListener = LocationListenerImpl()
 
-        mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, mlocListener)
+
+        mlocManager.requestLocationUpdates("gps", 0, 0f, mlocListener)
 
 
         val pkin = LatLng(52.219978, 21.011543)
