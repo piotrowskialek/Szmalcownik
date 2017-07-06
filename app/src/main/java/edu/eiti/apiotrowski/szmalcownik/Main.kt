7186f75c0036355f86b872c714b5f07db079102d
piotrowskialek <piotrowskialek@icloud.com>
2017-07-06 03:48:55 +0200

    permisiony todo front

--- app/src/main/java/edu/eiti/apiotrowski/szmalcownik/Main.kt
+++ app/src/main/java/edu/eiti/apiotrowski/szmalcownik/Main.kt
@@ -12,9 +12,24 @@ import com.google.android.gms.maps.model.CircleOptions
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import edu.eiti.apiotrowski.szmalcownik.model.LocationListenerImpl
+import android.Manifest.permission.WRITE_CALENDAR
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.util.Log
 
 
 class Main : FragmentActivity(), OnMapReadyCallback {
+
+
+    private val INITIAL_PERMS = arrayOf<String>(android.Manifest.permission.ACCESS_FINE_LOCATION)
+    private val CAMERA_PERMS = arrayOf<String>(android.Manifest.permission.CAMERA)
+    private val CONTACTS_PERMS = arrayOf<String>(android.Manifest.permission.READ_CONTACTS)
+    private val LOCATION_PERMS = arrayOf<String>(android.Manifest.permission.ACCESS_FINE_LOCATION)
+    private val INITIAL_REQUEST = 1337
+//    private val CAMERA_REQUEST = INITIAL_REQUEST + 1
+//    private val CONTACTS_REQUEST = INITIAL_REQUEST + 2
+//    private val LOCATION_REQUEST = INITIAL_REQUEST + 3
+
 
     private var mMap: GoogleMap? = null
 
@@ -25,6 +40,14 @@ class Main : FragmentActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        val permissionCheck = ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+
+        ActivityCompat.requestPermissions(this,
+                arrayOf<String>(android.Manifest.permission.ACCESS_FINE_LOCATION),INITIAL_REQUEST)
+
+
+//        requestPermissions(INITIAL_PERMS)
     }
 
 
